9b15b66bfd4ae009d7329455e5d1fe4e1c0f16fb
Denis Beliy <yuichirohakya227@gmail.com>
2022-03-11 18:21:39 +0200

    Обновил батник

--- 2exe.bat
+++ 2exe.bat
@@ -1,2 +1,2 @@
-pyinstaller ui_mainwindows.py --onefile --windowed --exclude-module _bootlocale --clean --add-data="assets\1.png;assets" --add-data="assets\2.png;assets" --add-data="resource_rc.py;."     
+pyinstaller ui_mainwindows.py --onefile --windowed --exclude-module _bootlocale --clean --add-data="assets\1.png;assets" --add-data="assets\2.png;assets" --add-data="resource_rc.py;." --add-data="assets\3.png;assets" --add-data="assets\4.png;assets"    
 pause
