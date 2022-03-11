34863783a6b868332a8fa572dae0fca13e75ef27
Denis Beliy <yuichirohakya227@gmail.com>
2022-03-11 14:52:27 +0200

    создал батник для pyinstaller, настроил работу с картинками

---
+++ 2exe.bat
@@ -0,0 +1,2 @@
+pyinstaller ui_mainwindows.py --onefile --windowed --exclude-module _bootlocale --clean --add-data="assets\1.png;assets" --add-data="assets\2.png;assets" --add-data="resource_rc.py;."     
+pause
---
+++ ui_mainwindows.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindowsjugytE.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+import sys
+import resource_rc
+
+
+class Ui_MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(857, 625)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(350, 10, 141, 61))
+        font = QFont()
+        font.setPointSize(24)
+        self.label.setFont(font)
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(200, 120, 431, 131))
+        font1 = QFont()
+        font1.setPointSize(20)
+        font1.setKerning(True)
+        self.label_2.setFont(font1)
+        self.label_2.setTextFormat(Qt.AutoText)
+        self.label_2.setScaledContents(False)
+        self.label_2.setAlignment(Qt.AlignCenter)
+        self.label_2.setWordWrap(True)
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(360, 530, 161, 31))
+        font2 = QFont()
+        font2.setPointSize(19)
+        self.label_3.setFont(font2)
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(320, 390, 211, 51))
+        font3 = QFont()
+        font3.setPointSize(15)
+        self.pushButton.setFont(font3)
+        self.label_4 = QLabel(self.centralwidget)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(-70, 220, 261, 381))
+        self.label_4.setPixmap(QPixmap(u":/newPrefix/assets/1.png"))
+        self.label_5 = QLabel(self.centralwidget)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(540, 220, 301, 371))
+        self.label_5.setPixmap(QPixmap(u":/newPrefix/assets/2.png"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u0425\u041f\u0424\u041a \u041e\u041f", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0431\u043b\u0456\u043a \u0437\u0430\u043c\u043e\u0432\u043b\u0435\u043d\u044c \u0430\u0442\u0435\u043b\u044c\u0454 \u0437 \u043f\u043e\u0448\u0438\u0442\u0442\u044f \u0442\u0430 \u0440\u0435\u043c\u043e\u043d\u0442\u0443 \u043e\u0434\u044f\u0433\u0443", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u0425\u0435\u0440\u0441\u043e\u043d 2022", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u041f\u0435\u0440\u0435\u0439\u0442\u0438 \u0434\u043e \u043c\u0435\u043d\u044e", None))
+        self.label_4.setText("")
+        self.label_5.setText("")
+    # retranslateUi
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = Ui_MainWindow()
+    widget.show()
+    
+
+    try:
+        sys.exit(app.exec_())
+    except:
+        print('exiting..')
