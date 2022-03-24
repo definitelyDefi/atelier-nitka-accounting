b1e215e14614351cb127ca417862202c4084423d
Denis Beliy <yuichirohakya227@gmail.com>
2022-03-24 16:25:33 +0200

    Добавил меню клиентов и форму добавления клиента

--- ui_mainwindows.py
+++ ui_mainwindows.py
@@ -12,9 +12,16 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 import sys
-import resource_rc
+import resource_rc              # импорт ресурсов 
+                                # создается с помощью      pyrcc5 -o resources.qrc resource_rc.py
 
 
+def exitfunc():
+    print('exiting...')
+    sys.exit()
+
+
+# Стартовое меню
 class start(QMainWindow):
 
     def __init__(self):
@@ -106,17 +113,22 @@ class start(QMainWindow):
         self.label_5.setText("")
 
 
+
+# Главное меню
 class main_menu(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.tostart.clicked.connect(self.tostartfunc)
-        self.exit.clicked.connect(self.exitfunc)
+        self.exit.clicked.connect(exitfunc)
+        self.clients.clicked.connect(self.toclientsfunc)
 
-    def exitfunc(self):
-        print('exiting...')
-        sys.exit()
+    def toclientsfunc(self):
+        self.cams = clients()
+        self.cams.show()
+        self.close()
+
 
     def tostartfunc(self):
         self.cams = start()
@@ -192,6 +204,160 @@ class main_menu(QMainWindow):
         self.label_2.setText("")
 
 
+
+
+# Меню клиентов
+
+class clients(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.back.clicked.connect(self.to_main_menu_func)
+        self.exit.clicked.connect(exitfunc)
+        self.add_new_client.clicked.connect(self.to_add_clientfunc)
+
+    def to_add_clientfunc(self):
+        self.cams = add_client()
+        self.cams.show()
+        self.close() 
+
+    def to_main_menu_func(self):
+        self.cams = main_menu()
+        self.cams.show()
+        self.close() 
+
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(832, 300)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.add_new_client = QPushButton(self.centralwidget)
+        self.add_new_client.setObjectName(u"add_new_client")
+        self.add_new_client.setGeometry(QRect(60, 140, 161, 31))
+        self.view_clients = QPushButton(self.centralwidget)
+        self.view_clients.setObjectName(u"view_clients")
+        self.view_clients.setGeometry(QRect(330, 140, 161, 31))
+        self.clients_zvit = QPushButton(self.centralwidget)
+        self.clients_zvit.setObjectName(u"clients_zvit")
+        self.clients_zvit.setGeometry(QRect(620, 140, 161, 31))
+        self.back = QPushButton(self.centralwidget)
+        self.back.setObjectName(u"back")
+        self.back.setGeometry(QRect(0, 0, 41, 31))
+        icon = QIcon()
+        icon.addFile(u":/newPrefix/assets/arrow.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.back.setIcon(icon)
+        self.back.setIconSize(QSize(25, 25))
+        self.back.setCheckable(False)
+        self.back.setFlat(False)
+        self.exit = QPushButton(self.centralwidget)
+        self.exit.setObjectName(u"exit")
+        self.exit.setGeometry(QRect(790, 0, 41, 31))
+        icon1 = QIcon()
+        icon1.addFile(u":/newPrefix/assets/exit.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.exit.setIcon(icon1)
+        self.exit.setIconSize(QSize(25, 25))
+        self.exit.setCheckable(False)
+        self.exit.setFlat(False)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.add_new_client.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0434\u0430\u0442\u0438 \u043d\u043e\u0432\u043e\u0433\u043e \u043a\u043b\u0456\u0454\u043d\u0442\u0430", None))
+        self.view_clients.setText(QCoreApplication.translate("MainWindow", u"\u041f\u0435\u0440\u0435\u0433\u043b\u044f\u0434 \u043a\u043b\u0456\u0454\u043d\u0442\u0456\u0432", None))
+        self.clients_zvit.setText(QCoreApplication.translate("MainWindow", u"\u0417\u0432\u0456\u0442\u043d\u0456\u0441\u0442\u044c \u043f\u043e \u043a\u043b\u0456\u0454\u0442\u0430\u043c", None))
+        self.back.setText("")
+        self.exit.setText("")
+    # retranslateUi
+
+#TODO: Сделать отправку запроса в базу, отладку ошибок
+class add_client(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.back.clicked.connect(self.to_clients)
+        self.exit.clicked.connect(exitfunc)
+    
+    def to_clients(self):
+        self.cams = clients()
+        self.cams.show()
+        self.close()  
+
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(569, 372)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.name = QLineEdit(self.centralwidget)
+        self.name.setObjectName(u"name")
+        self.name.setGeometry(QRect(190, 130, 261, 41))
+        font = QFont()
+        font.setPointSize(12)
+        self.name.setFont(font)
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 140, 161, 31))
+        font1 = QFont()
+        font1.setPointSize(11)
+        self.label.setFont(font1)
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 190, 161, 31))
+        self.label_2.setFont(font1)
+        self.phone = QLineEdit(self.centralwidget)
+        self.phone.setObjectName(u"phone")
+        self.phone.setGeometry(QRect(190, 190, 261, 31))
+        self.phone.setFont(font)
+        self.submit = QPushButton(self.centralwidget)
+        self.submit.setObjectName(u"submit")
+        self.submit.setGeometry(QRect(350, 250, 101, 31))
+        self.submit.setFont(font1)
+        self.back = QPushButton(self.centralwidget)
+        self.back.setObjectName(u"back")
+        self.back.setGeometry(QRect(0, 0, 41, 31))
+        font2 = QFont()
+        font2.setPointSize(2)
+        self.back.setFont(font2)
+        icon = QIcon()
+        icon.addFile(u":/newPrefix/assets/arrow.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.back.setIcon(icon)
+        self.back.setIconSize(QSize(25, 25))
+        self.exit = QPushButton(self.centralwidget)
+        self.exit.setObjectName(u"exit")
+        self.exit.setGeometry(QRect(530, 0, 41, 31))
+        self.exit.setFont(font2)
+        icon1 = QIcon()
+        icon1.addFile(u":/newPrefix/assets/exit.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.exit.setIcon(icon1)
+        self.exit.setIconSize(QSize(25, 25))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u0412\u0432\u0435\u0434\u0456\u0442\u044c \u041f\u0406\u0411 \u043a\u043b\u0456\u0454\u043d\u0442\u0430", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u0412\u0432\u0435\u0434\u0456\u0442\u044c \u043d\u043e\u043c\u0435\u0440 \u043a\u043b\u0456\u0454\u043d\u0442\u0430", None))
+        self.submit.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0434\u0430\u0442\u0438", None))
+        self.back.setText("")
+        self.exit.setText("")
+    # retranslateUi
 
 if __name__ == "__main__":
 
